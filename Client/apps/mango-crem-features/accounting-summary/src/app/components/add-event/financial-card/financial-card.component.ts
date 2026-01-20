9f8c2c491185d897ab1a99b25d743764a3721d06
Derick Stinson <dstinson@Costargroup.com>
2026-01-20 10:03:48 -0500

    Merged PR 22015: Merged PR 21782: 157094 - Disable Override Profile checkbox on Full Termination
    Merged PR 21782: 157094 - Disable Override Profile checkbox on Full Termination
    
    157094 - Disable Override Profile checkbox on Full Termination
    
    Related work items: #157094
    
    Related work items: #157094

--- Client/apps/mango-crem-features/accounting-summary/src/app/components/add-event/financial-card/financial-card.component.ts
+++ Client/apps/mango-crem-features/accounting-summary/src/app/components/add-event/financial-card/financial-card.component.ts
@@ -1837,11 +1837,6 @@ export class FinancialCardComponent implements OnChanges, OnInit, OnDestroy {
       }
       case 'amortization': {
         this.amortizationCompositeDropdownTouched = true;
-        if (this.financialForm.get('amortizationProfile').value !== null) {
-          this.financialForm.get('overrideAmortizationProfile').enable();
-        } else {
-          this.financialForm.get('overrideAmortizationProfile').disable();
-        }
         break;
       }
       case 'currency': {
