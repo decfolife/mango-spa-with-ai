4c97f5c99130dab53777ff21c59743e66b4452ab
Michael Werve <mwerve@costargroup.com>
2026-03-02 10:13:17 -0500

    Merged PR 22203: add dr environment files for CA and mango-spa
    add dr environment files for CA and mango-spa
    
    Related work items: #158947

---
+++ Client/apps/mango/src/environments/environment.dr.ts
@@ -0,0 +1,14 @@
+import { Environment } from '@mango/data-models/lib-data-models';
+
+class EnvironmentsCrem implements Environment {
+  production = true;
+  name = 'DR';
+  showPayload = false;
+  cremBaseUrl = 'https://[CLIENT].dr.costarremanager.com';
+  CAUrl = 'https://login.dr.costarremanager.com';
+  // Only needed for localhost. Otherwise use `${window.location.origin}/api`
+  baseApiUrl = 'https://api.dr.costarremanager.com/';
+  logRocketAppId = '6vmxfr/spa-prod-zjinp';
+}
+
+export const environment = new EnvironmentsCrem();
